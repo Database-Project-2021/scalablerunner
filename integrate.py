7dbec00fb03a622c1bf1ea484a1e1431cf048d87
FrankCCCCC <unaxultraspaceos5@gmail.com>
2021-09-28 16:06:38 +0800

    fixed PopCat bugs, now it can run DBRunner with TaskRunner

--- integrate.py
+++ integrate.py
@@ -31,6 +31,10 @@ def get_host_infos():
 
     return hostname, username, password, port
 
+def name_fn(reports_path: str, alts: dict):
+    rte = alts['vanillabench']['org.vanilladb.bench.BenchmarkerParameters.NUM_RTES']
+    return os.path.join(reports_path, f'google_rte-{rte}')
+
 # def config_db_runner(db_runner: DBRunner) -> DBRunner:
 #     db_runner.config_bencher(sequencer="192.168.1.32", 
 #                              servers=["192.168.1.31", "192.168.1.30", "192.168.1.27", "192.168.1.26"], 
@@ -39,46 +43,53 @@ def get_host_infos():
 #     db_runner.config_cluster(server_count=4, jar_dir='latest')
 #     return db_runner
 
-# def get_workspace_name():
-#     return 'db_runner_workspace_test'
-
 class PopCat(BaseClass):
-    # MICRO = '1'
-    # TPCC = '2'
-    # TPCE = '3'
-    # YCSB = '4'
-
-    # ESTIMATOR_PY = 'cost_est.py'
-
-    # LOCAL = 'local'
-    # REMOTE = 'remote'
-
-    def __init__(self, reports_path: str, bench_type: str):
+    YCSB = 0
+    def __init__(self, reports_path: str, bench_type: str, workspace: str):
         self.reports_path = reports_path
         self.bench_type = bench_type
-
-    def init(self, server_count: int, sequencer: str, servers: list, clients: list, server_jar: str, client_jar: str, workspace: str,
-             package_path: str='/home/db-under/sychou/autobench/package/jdk-8u211-linux-x64.tar.gz', 
-             server_client_ratio: float=None, max_server_per_machine: int=None, max_client_per_machine: int=None, jar_dir: str='latest'):
-        HOSTNAME, USERNAME, PASSWORD, PORT = get_host_infos()
-
         self.dr = DBRunner(workspace)
+
+    def connect(self):
+        HOSTNAME, USERNAME, PASSWORD, PORT = get_host_infos()
         self.dr.connect(hostname=HOSTNAME, username=USERNAME, password=PASSWORD, port=PORT)
-        # self.dr = config_db_runner(self.dr)
+    
+    def close(self):
+        self.dr.close()
+
+    def config(self, server_count: int=None, sequencer: str=None, servers: list=None, clients: list=None, package_path: str=None, 
+               server_client_ratio: float=None, max_server_per_machine: int=None, max_client_per_machine: int=None, jar_dir: str='latest'):
+
+        self.__config(server_count=server_count, sequencer=sequencer, servers=servers, clients=clients, package_path=package_path,
+                      server_client_ratio=server_client_ratio, max_server_per_machine=max_server_per_machine, max_client_per_machine=max_client_per_machine, jar_dir=jar_dir)
+
+    def __config(self, server_count: int, sequencer: str, servers: list, clients: list,package_path: str, 
+               server_client_ratio: float, max_server_per_machine: int, max_client_per_machine: int, jar_dir: str):
+             
         self.dr.config_bencher(sequencer=sequencer, servers=servers, clients=clients, package_path=package_path)
         self.dr.config_cluster(server_count=server_count, server_client_ratio=server_client_ratio, max_server_per_machine=max_server_per_machine, max_client_per_machine=max_client_per_machine, jar_dir=jar_dir)
+
+    def init(self, server_jar: str, client_jar: str):
+        self.connect()
+        # self.dr = config_db_runner(self.dr)
         self.dr.init()
         self.dr.upload_jars(server_jar=server_jar, client_jar=client_jar, use_stable=True)
+        self.close()
 
-    def load(self, alts: dict):
-        self.dr.load(is_kill_java=True, alts=alts)
+    def load(self, alts: dict=None, base_config: str=None):
+        self.connect()
+        self.dr.load(is_kill_java=True, alts=alts, base_config=base_config)
+        self.close()
 
-    def init_load(self, server_count: int, sequencer: str, servers: list, clients: list, server_jar: str, client_jar: str, workspace: str, alts: dict):
-        self.init(server_count=server_count, sequencer=sequencer, servers=servers, clients=clients, server_jar=server_jar, client_jar=client_jar, workspace=workspace)
-        self.load(alts=alts)
+    def init_load(self, server_jar: str, client_jar: str, base_config: str=None, alts: dict=None):
+        self.init( server_jar=server_jar, client_jar=client_jar)
+        self.load(alts=alts, base_config=base_config)
 
-    def benchmark(self, name: str, alts: dict, event: str):
-        self.dr.bench(reports_path=self.reports_path, alts=alts)
+    def benchmark(self, name_fn: callable, alts: dict=None, base_config: str=None, event: str=None):
+        self.connect()
+        rp_path = name_fn(reports_path=self.reports_path, alts=alts)
+        self.dr.bench(is_kill_java=True, reports_path=rp_path, alts=alts, base_config=base_config)
+        self.close()
 
     def __remote_estimator(self):
         """
@@ -97,10 +108,6 @@ class PopCat(BaseClass):
         self.__local_estimator(**kwargs)
 
     def remote_estimator(self, hostname: str, username: str, passward: str, port: int, event: str, **kwargs) -> None:
-        # self.__remote_estimator(*args, **kwargs)
-        # ssh = SSH(hostname=hostname, username=username, passward=passward, port=port)
-        # ssh.connect()
-        # ssh.put(files='cost_est.py', remote_path='estimator.py')
         for server in kwargs['remote_servers']:
             conn = rpyc.classic.connect(server)
             fn = conn.teleport(self.__remote_estimator)
@@ -118,120 +125,127 @@ class Socket():
         pass
 
 cw = {
-    'sequencer': "192.168.1.20",
-    'servers': ["192.168.1.16", "192.168.1.17", "192.168.1.23", "192.168.1.19"],
-    'clients': ["192.168.1.3", "192.168.1.4"],
     'workspace': 'db_runner_workspace_cw',
-    'benc_alts': {
-                    'vanillabench': {
-                        'org.vanilladb.bench.BenchmarkerParameters.BENCHMARK_INTERVAL': "30000"
-                    },
-                    'elasql': {
-                        'org.elasql.perf.tpart.ai.Estimator.ENABLE_COLLECTING_DATA': "true"
-                    },
-                    'elasqlbench': {
-                        'org.elasql.bench.benchmarks.ycsb.ElasqlYcsbConstants.INIT_RECORD_PER_PART': "100000",
-                        'org.elasql.bench.benchmarks.ycsb.ElasqlYcsbConstants.RW_TX_RATE': "0"
-                    }
-                }
-}
-
-sy = {
+    'package_path': '/home/db-under/sychou/autobench/package/jdk-8u211-linux-x64.tar.gz',
+    # New
+    # 'sequencer': "192.168.1.6",
+    # 'servers': ["192.168.1.15", "192.168.1.16", "192.168.1.17", "192.168.1.27"],
+    # 'clients': ["192.168.1.30", "192.168.1.31"],
+    # SY
     'sequencer': "192.168.1.32", 
     'servers': ["192.168.1.31", "192.168.1.30", "192.168.1.27", "192.168.1.26"], 
     'clients': ["192.168.1.9", "192.168.1.8"], 
-    'workspace': 'db_runner_workspace_sy',
-    'benc_alts': {
-                    'vanillabench': {
-                        'org.vanilladb.bench.BenchmarkerParameters.BENCHMARK_INTERVAL': "30000"
-                    },
-                    'elasql': {
-                        'org.elasql.perf.tpart.ai.Estimator.ENABLE_COLLECTING_DATA': "true"
-                    },
-                    'elasqlbench': {
-                        'org.elasql.bench.benchmarks.ycsb.ElasqlYcsbConstants.INIT_RECORD_PER_PART': "100000",
-                        'org.elasql.bench.benchmarks.ycsb.ElasqlYcsbConstants.RW_TX_RATE': "1"
-                    }
-                }
+    'load_alts': [None],
+    'load_base_config': ['temp/smg/load.smg.toml'],
+    'bench_alts': [
+        {
+            'vanillabench':{
+                'org.vanilladb.bench.BenchmarkerParameters.NUM_RTES': "50",
+            },
+        },
+        {
+            'vanillabench':{
+                'org.vanilladb.bench.BenchmarkerParameters.NUM_RTES': "75",
+            },
+        },
+        {
+            'vanillabench':{
+                'org.vanilladb.bench.BenchmarkerParameters.NUM_RTES': "100",
+            },
+        },
+        {
+            'vanillabench':{
+                'org.vanilladb.bench.BenchmarkerParameters.NUM_RTES': "125",
+            },
+        },
+        {
+            'vanillabench':{
+                'org.vanilladb.bench.BenchmarkerParameters.NUM_RTES': "150",
+            },
+        },
+        {
+            'vanillabench':{
+                'org.vanilladb.bench.BenchmarkerParameters.NUM_RTES': "175",
+            },
+        },
+        {
+            'vanillabench':{
+                'org.vanilladb.bench.BenchmarkerParameters.NUM_RTES': "200",
+            },
+        }
+    ],
+    'bench_base_config': ['temp/smg/bench.smg.toml']
+#     'benc_alts': {
+#                     'vanillabench': {
+#                         'org.vanilladb.bench.BenchmarkerParameters.BENCHMARK_INTERVAL': "30000"
+#                     },
+#                     'elasql': {
+#                         'org.elasql.perf.tpart.ai.Estimator.ENABLE_COLLECTING_DATA': "true"
+#                     },
+#                     'elasqlbench': {
+#                         'org.elasql.bench.benchmarks.ycsb.ElasqlYcsbConstants.INIT_RECORD_PER_PART': "100000",
+#                         'org.elasql.bench.benchmarks.ycsb.ElasqlYcsbConstants.RW_TX_RATE': "0"
+#                     }
+#                 }
 }
 
-wy = {
-    'sequencer': "192.168.1.32", 
-    'servers': ["192.168.1.31", "192.168.1.30", "192.168.1.27", "192.168.1.26"], 
-    'clients': ["192.168.1.9", "192.168.1.8"], 
-    'workspace': 'db_runner_workspace_wy',
-    'benc_alts': {
-                    'vanillabench': {
-                        'org.vanilladb.bench.BenchmarkerParameters.BENCHMARK_INTERVAL': "30000"
-                    },
-                    'elasql': {
-                        'org.elasql.perf.tpart.ai.Estimator.ENABLE_COLLECTING_DATA': "true"
-                    },
-                    'elasqlbench': {
-                        'org.elasql.bench.benchmarks.ycsb.ElasqlYcsbConstants.INIT_RECORD_PER_PART': "100000",
-                        'org.elasql.bench.benchmarks.ycsb.ElasqlYcsbConstants.RW_TX_RATE': "0.5"
-                    }
-                }
-}
+# sy = {
+#     'sequencer': "192.168.1.32", 
+#     'servers': ["192.168.1.31", "192.168.1.30", "192.168.1.27", "192.168.1.26"], 
+#     'clients': ["192.168.1.9", "192.168.1.8"], 
+#     'workspace': 'db_runner_workspace_sy',
+#     'benc_alts': {
+#                     'vanillabench': {
+#                         'org.vanilladb.bench.BenchmarkerParameters.BENCHMARK_INTERVAL': "30000"
+#                     },
+#                     'elasql': {
+#                         'org.elasql.perf.tpart.ai.Estimator.ENABLE_COLLECTING_DATA': "true"
+#                     },
+#                     'elasqlbench': {
+#                         'org.elasql.bench.benchmarks.ycsb.ElasqlYcsbConstants.INIT_RECORD_PER_PART': "100000",
+#                         'org.elasql.bench.benchmarks.ycsb.ElasqlYcsbConstants.RW_TX_RATE': "1"
+#                     }
+#                 }
+# }
 
 package_path = '/home/db-under/sychou/autobench/package/jdk-8u211-linux-x64.tar.gz'
 server_jar = 'data/jars/server.jar'
 client_jar = 'data/jars/client.jar'
 server_count = 4
-load_alts = {
-                'elasqlbench': {
-                    'org.elasql.bench.benchmarks.ycsb.ElasqlYcsbConstants.INIT_RECORD_PER_PART': "100000"
-                }
-            }
+# load_alts = {
+#                 'elasqlbench': {
+#                     'org.elasql.bench.benchmarks.ycsb.ElasqlYcsbConstants.INIT_RECORD_PER_PART': "100000"
+#                 }
+#             }
 
 if __name__ == '__main__':
-    pc = PopCat(reports_path='temp/integrate', bench_type=PopCat.YCSB)
-    # pc.init_load(server_count=server_count, sequencer=sy['sequencer'], servers=sy['servers'], clients=sy['clients'], 
-    #              workspace=sy['workspace'], server_jar=server_jar, client_jar=server_jar, alts=load_alts)
-    # pc.benchmark(alts=sy['benc_alts'])
+    pc = PopCat(reports_path='temp/integrate', bench_type=PopCat.YCSB, workspace=cw['workspace'])
+    pc.config(server_count=1, sequencer=cw['sequencer'], servers=cw['servers'], clients=cw['clients'])
+
+    # pc.init_load(server_jar=server_jar, client_jar=server_jar, alts=cw['load_alts'][0], base_config=cw['load_base_config'][0])
+    # pc.benchmark(name_fn=name_fn, alts=cw['bench_alts'][0], base_config=cw['bench_base_config'][0])
 
     config = {
-        'Section Initialize': {
+        'Section Initialize | 1 server': {
             'Group CW': {
                 'Call': pc.init_load,
                 'Param': {
-                    'server_count': [server_count],
-                    'sequencer': [cw['sequencer']],
-                    'servers': [cw['servers']],
-                    'clients': [cw['clients']],
-                    'workspace': [cw['workspace']],
-                    'server_jar': [server_jar],
-                    'client_jar': [server_jar],
-                    'alts': [load_alts]
+                    'server_jar': [server_jar], 
+                    'client_jar': [client_jar],
+                    'alts': cw['load_alts'],
+                    'base_config': cw['load_base_config'],
                 }
             },
-            'Group SY': {
-                'Call': pc.init_load,
-                'Param': {
-                    'server_count': [server_count],
-                    'sequencer': [sy['sequencer']],
-                    'servers': [sy['servers']],
-                    'clients': [sy['clients']],
-                    'workspace': [sy['workspace']],
-                    'server_jar': [server_jar],
-                    'client_jar': [server_jar],
-                    'alts': [load_alts]
-                }
-            }
         },
-        'Section Benchmark': {
+        'Section Benchmark | 1 server': {
             'Group CW': {
                 'Call': pc.benchmark,
                 'Param': {
-                    'alts': [cw['benc_alts']]
+                    'name_fn': [name_fn],
+                    'alts': cw['bench_alts'],
+                    'base_config': cw['bench_base_config']
                 }
             },
-            'Group SY': {
-                'Call': pc.benchmark,
-                'Param': {
-                    'alts': [sy['benc_alts']]
-                }
-            }
         }
     }
 
